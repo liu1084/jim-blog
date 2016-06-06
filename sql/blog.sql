2287223d53f3fdbdc67ebb73d7343a6b0a41e2e5
Jim Liu <liu1084@163.com>
2016-06-07 01:27:42 +0800

    update

--- sql/blog.sql
+++ sql/blog.sql
@@ -15,32 +15,97 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`blog` /*!40100 DEFAULT CHARACTER SET ut
 
 USE `blog`;
 
-/*Table structure for table `admin` */
+/*Table structure for table `articles` */
 
-DROP TABLE IF EXISTS `admin`;
+DROP TABLE IF EXISTS `articles`;
 
-CREATE TABLE `admin` (
+CREATE TABLE `articles` (
+  `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `userId` bigint(20) NOT NULL DEFAULT '0',
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `isDraft` tinyint(1) NOT NULL DEFAULT '0',
+  `isPrivate` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `articles` */
+
+insert  into `articles`(`id`,`title`,`content`,`userId`,`create_at`,`update_at`,`isDraft`,`isPrivate`) values (1,'task1','dddddddddddddddddddddddddd',1,'2016-06-05 00:40:35','2016-06-05 00:40:35',0,0);
+
+/*Table structure for table `comments` */
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
+  `parentId` bigint(30) DEFAULT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `userId` bigint(30) NOT NULL,
+  `articleId` bigint(30) NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `isLocked` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `comments` */
+
+insert  into `comments`(`id`,`parentId`,`title`,`content`,`userId`,`articleId`,`create_at`,`update_at`,`isLocked`) values (1,NULL,'task comment 1','/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n/*!40101 SET NAMES utf8 */;\r\n/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;\r\n/*!40103 SET TIME_ZONE=\'+00:00\' */;\r\n/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;\r\n/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;\r\n/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=\'NO_AUTO_VALUE_ON_ZERO\' */;\r\n/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;\r\n\r\n...........',1,1,'2016-06-05 00:41:59','2016-06-05 00:41:59',0);
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL DEFAULT '',
-  `password` varchar(50) NOT NULL DEFAULT '',
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `description` varchar(100) NOT NULL DEFAULT '',
   `isActive` tinyint(2) NOT NULL DEFAULT '1',
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `roles` */
+
+insert  into `roles`(`id`,`name`,`description`,`isActive`,`create_at`,`update_at`) values (1,'admin','admin',1,'2016-06-05 00:39:41','2016-06-05 00:39:41');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userId` bigint(30) NOT NULL,
+  `roleId` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `admin` */
+/*Data for the table `user_role` */
 
-/*Table structure for table `user` */
+/*Table structure for table `users` */
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(50) NOT NULL DEFAULT '',
+  `roleId` int(10) NOT NULL,
+  `nickname` varchar(50) NOT NULL DEFAULT '',
+  `isLocked` tinyint(1) NOT NULL DEFAULT '0',
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-/*Data for the table `user` */
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`roleId`,`nickname`,`isLocked`,`create_at`,`update_at`) values (1,'liujun','111',0,'Jim',0,'2016-06-05 10:42:38','2016-06-05 10:42:38'),(2,'liqing','222',1,'Sunny',0,'2016-06-05 00:42:38','2016-06-05 00:42:38');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
