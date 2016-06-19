130598f91c9f3f99136661e2f23c35b00fbdbaac
jim.liu <liu1084@163.com>
2016-06-20 02:59:56 +0800

    update: user & role many-to-many relationship

--- sql/blog.sql
+++ sql/blog.sql
@@ -23,13 +23,15 @@ CREATE TABLE `articles` (
   `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL DEFAULT '',
   `content` text NOT NULL,
-  `userId` bigint(20) NOT NULL DEFAULT '0',
+  `userId` bigint(30) unsigned NOT NULL DEFAULT '1',
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `isDraft` tinyint(1) NOT NULL DEFAULT '0',
   `isPrivate` tinyint(1) NOT NULL DEFAULT '0',
   `isActive` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `FK_articles_users` (`userId`),
+  CONSTRAINT `FK_articles_users` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Data for the table `articles` */
@@ -42,10 +44,12 @@ DROP TABLE IF EXISTS `articles_metadata`;
 
 CREATE TABLE `articles_metadata` (
   `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
-  `articleId` bigint(30) NOT NULL DEFAULT '1',
+  `articleId` bigint(30) unsigned NOT NULL DEFAULT '1',
   `key` varchar(50) NOT NULL DEFAULT '',
   `value` text NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `FK_articles_metadata` (`articleId`),
+  CONSTRAINT `FK_articles_metadata` FOREIGN KEY (`articleId`) REFERENCES `articles` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `articles_metadata` */
@@ -56,15 +60,19 @@ DROP TABLE IF EXISTS `comments`;
 
 CREATE TABLE `comments` (
   `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
-  `parentId` bigint(30) DEFAULT NULL,
+  `parentId` bigint(30) unsigned DEFAULT NULL,
   `title` varchar(100) NOT NULL,
   `content` text NOT NULL,
-  `userId` bigint(30) NOT NULL,
-  `articleId` bigint(30) NOT NULL,
+  `userId` bigint(30) unsigned NOT NULL,
+  `articleId` bigint(30) unsigned NOT NULL,
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `isActive` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `FK_comments_articles` (`articleId`),
+  KEY `FK_comments_users` (`userId`),
+  CONSTRAINT `FK_comments_articles` FOREIGN KEY (`articleId`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_comments_users` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `comments` */
@@ -77,10 +85,12 @@ DROP TABLE IF EXISTS `comments_metadata`;
 
 CREATE TABLE `comments_metadata` (
   `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
-  `commentId` bigint(30) NOT NULL DEFAULT '1',
+  `commentId` bigint(30) unsigned NOT NULL DEFAULT '1',
   `key` varchar(50) NOT NULL DEFAULT '',
   `value` text NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `FK_comments_metadata` (`commentId`),
+  CONSTRAINT `FK_comments_metadata` FOREIGN KEY (`commentId`) REFERENCES `comments` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `comments_metadata` */
@@ -97,11 +107,11 @@ CREATE TABLE `roles` (
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `roles` */
 
-insert  into `roles`(`id`,`name`,`description`,`isActive`,`create_at`,`update_at`) values (1,'admin','admin',1,'2016-06-05 00:39:41','2016-06-05 00:39:41');
+insert  into `roles`(`id`,`name`,`description`,`isActive`,`create_at`,`update_at`) values (1,'admin','admin',1,'2016-06-05 00:39:41','2016-06-05 00:39:41'),(2,'user','my wife',1,'2016-06-20 02:31:51','2016-06-20 02:31:51');
 
 /*Table structure for table `user_role` */
 
@@ -109,12 +119,18 @@ DROP TABLE IF EXISTS `user_role`;
 
 CREATE TABLE `user_role` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `userId` bigint(30) NOT NULL,
-  `roleId` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `userId` bigint(30) unsigned NOT NULL,
+  `roleId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_user_role_users` (`userId`),
+  KEY `FK_user_role_roles` (`roleId`),
+  CONSTRAINT `FK_user_role_roles` FOREIGN KEY (`roleId`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_user_role_users` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user_role` */
+
+insert  into `user_role`(`id`,`userId`,`roleId`) values (1,1,1),(2,1,2);
 
 /*Table structure for table `users` */
 
@@ -124,9 +140,8 @@ CREATE TABLE `users` (
   `id` bigint(30) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL DEFAULT '',
   `password` varchar(50) NOT NULL DEFAULT '',
-  `roleId` int(10) NOT NULL,
   `nickname` varchar(50) NOT NULL DEFAULT '',
-  `isLocked` tinyint(1) NOT NULL DEFAULT '0',
+  `isActive` tinyint(1) NOT NULL DEFAULT '1',
   `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -134,7 +149,7 @@ CREATE TABLE `users` (
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`username`,`password`,`roleId`,`nickname`,`isLocked`,`create_at`,`update_at`) values (1,'liujun','111',0,'Jim',0,'2016-06-05 10:42:38','2016-06-05 10:42:38'),(2,'liqing','222',1,'Sunny',0,'2016-06-05 00:42:38','2016-06-05 00:42:38');
+insert  into `users`(`id`,`username`,`password`,`nickname`,`isActive`,`create_at`,`update_at`) values (1,'liujun','111','Jim',1,'2016-06-05 10:42:38','2016-06-05 10:42:38'),(2,'liqing','222','Sunny',1,'2016-06-05 00:42:38','2016-06-05 00:42:38');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
--- src/main/java/com/jim/mapper/UserRoleMapper.java
+++ src/main/java/com/jim/mapper/UserRoleMapper.java
@@ -11,16 +11,14 @@
 
 package com.jim.mapper;
 
+import com.jim.model.Role;
+import com.jim.model.User;
+
 import java.util.List;
 
-import com.jim.model.Role;
-
-/**
- * Created by jim.liu on 2016-06-17.
- * This class is ...
- */
 public interface UserRoleMapper {
-	List<Role> getRoles(long id);
-    int addRole(long userId, long roleId);
     int deleteRole(long userId, long roleId);
+    int addRole(long userId, long roleId);
+    List<Role> getRoles(long userId);
+    List<User> getUsers(long roleId);
 }
